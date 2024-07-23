ec1b066dc791db0ada08c0ff83889b98422351ff
honue <zouhy2001@gmail.com>
2024-07-23 16:04:04 +0800

    cdtransfer 0.1.1

--- plugins/cdtransfer/__init__.py
+++ plugins/cdtransfer/__init__.py
@@ -27,7 +27,7 @@ class CDTransfer(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/honue/MoviePilot-Plugins/main/icons/clouddrive.png"
     # 插件版本
-    plugin_version = "0.1"
+    plugin_version = "0.1.1"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
@@ -126,7 +126,7 @@ class CDTransfer(_PluginBase):
     def task(self):
         with lock:
             waiting_process_list = self.get_data('waiting_process_list') or []
-            logger.info(f'开始执行上传任务 {waiting_process_list}')
+            logger.debug(f'开始执行上传任务 {waiting_process_list}')
             process_list = waiting_process_list.copy()
             for file in waiting_process_list:
                 process_list.remove(file) if self._upload_file(file) else None
@@ -140,12 +140,13 @@ class CDTransfer(_PluginBase):
             dest_path = file_path.replace(self._local_media_prefix_path, self._cd_media_prefix_path)
             # folder /115/emby/series/日韩剧/财阀X刑警 (2024)/Season 1/  file_name 财阀X刑警 - S01E12 - 第 12 集.mkv
             folder, file_name = os.path.split(dest_path)
+            folder = folder + '/'
             if not self._fs.exists(folder):
                 self._fs.mkdir(folder)
                 logger.info(f'创建文件夹 {folder}')
             self._fs.chdir(folder)
             # 将本地媒体库文件上传
-            self._fs.upload(file_name)
+            self._fs.upload(file_path)
             logger.info(f'成功上传 {file_name} 至 {dest_path}')
             return True
         except Exception as e:
